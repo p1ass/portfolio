45d4efdc8c49527768b8d9da48b81cc674a23e3a
Naoki Kishi <naokikishi.kyoto@gmail.com>
2019-04-03 10:40:48 +0900

    Change workbox style

---
+++ components/workBox.vue
@@ -0,0 +1,47 @@
+<template>
+  <section class="work-box">
+    <a :href="item.link" target="_blank" rel="noopener noreferrer">
+      <h2>{{ item.name }}</h2>
+    </a>
+    <p class="box-content">
+      {{ item.content }}
+    </p>
+    <p class="sub-content">
+      {{ item.subContent }}
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'WorkBox',
+  props: {
+    item: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/styles/_color.scss';
+@import '@/assets/styles/_mixin.scss';
+
+.work-box {
+  @include box-shadow;
+  @include box(300px, 300px);
+}
+
+h2 {
+  @include box-title;
+}
+
+.box-content {
+  @include box-content;
+}
+
+.sub-content {
+  @include box-sub-content;
+}
+</style>
--- pages/works.vue
+++ pages/works.vue
@@ -2,18 +2,14 @@
   <article class="works-wrapper">
     <pageTitle title="Works" />
     <div class="works">
-      <box v-for="work in works" :key="work.link" :item="work">
-        <a :href="work.link" target="_blank" rel="noopener noreferrer">
-          URL
-        </a>
-      </box>
+      <work-box v-for="work in works" :key="work.link" :item="work" />
     </div>
   </article>
 </template>
 
 <script>
 import pageTitle from '@/components/pageTitle'
-import box from '@/components/box'
+import workBox from '@/components/workBox'
 
 export default {
   name: 'Works',
@@ -24,7 +20,7 @@ export default {
   },
   components: {
     pageTitle,
-    box
+    workBox
   },
   data() {
     return {
@@ -33,42 +29,49 @@ export default {
           name: 'feeder',
           content:
             '複数のRSSやAtomフィードなどから記事を取得し、一つのRSSやJSONを生成するライブラリです。インターフェースを実装することで、Qiitaなど任意の場所からデータを取得することができます。',
+          subContent: 'Go',
           link: 'https://github.com/naoki-kishi/feeder'
         },
         {
           name: 'GraphQL Chat Server',
           content:
             'GraphQL Subscriptionsを使ったリアルタイムチャットサーバです。Redis PubSubを使用してスケールアウトできる構成を目指しました。',
+          subContent: 'Go, GraphQL, Redis',
           link: 'https://github.com/naoki-kishi/graphql-redis-realtime-chat'
         },
         {
           name: 'えもじっく',
           content:
-            'AWS Rekognitionの顔認識機能を使って、人の画像を絵文字に変換するWebサービスです。フロントエンドにはNuxt.jsを、バックエンドにはPythonを使い、Serverless Frameworkで構築しています。',
+            '人の顔を絵文字スタンプに変換するWebサービスです。プライバシーを配慮した画像を簡単にソーシャルメディアにアップロードできます。',
+          subContent: 'Python, Nuxt.js, AWS',
           link: 'https://github.com/naoki-kishi/emojic.ch'
         },
         {
           name: 'marshiv',
           content:
             'pixivの2018年冬インターンシップに参加した際に作成した画像ビューワーです。Intersection Observerを使ったイラストの無限スクロースやタグを用いた簡易的なレコメンドシステムを作成しました。',
+          subContent: 'Javascript, Nuxt.js',
           link: 'https://plus-blog.hatenablog.com/entry/pixiv-intern-2018'
         },
         {
           name: 'Portfolio Site',
           content:
-            '今までフロントエンドの勉強をしたことがなかったので、勉強のために作成しました。デザインはiPhoneのApp StoreのようなドロップシャドウやInstagramのアイコンのようなグラデーションを意識しました。',
+            '私のポートフォリオです。iOSのApp Storeのようなドロップシャドウや、Instagramのアイコンのようなグラデーションを意識したデザインにしました。',
+          subContent: 'HTML, SCSS, Nuxt.js',
           link: 'https://github.com/naoki-kishi/portfolio'
         },
         {
           name: 'KU休講情報Bot',
           content:
             '京都大学の休講情報を定期的につぶやくBotです。EC2上でcronを使って動かしています。Pythonで書かれており、休講情報はSeleniumとChrome Driverを使ってスクレイピングで取得しています。',
+          subContent: 'Python, Selenium',
           link: 'https://github.com/naoki-kishi/KUCancelAnnouncementBot'
         },
         {
           name: 'SHABEL',
           content:
             'LINE SUMMER INTERNSHIP 2018 エンジニアスクールコースで作成したメッセージングアプリです。STOMP over Websocketを使った通信やローカルDBにメッセージを保存して永続化することができます。',
+          subContent: 'Kotlin, SpringBoot',
           link: 'https://github.com/line-school2018summer/kyoto-a-client'
         }
       ]
