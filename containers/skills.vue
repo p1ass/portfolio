8fc5060f1983b77ba063c108116af03b50a14bfc
Naoki Kishi <naokikishi.kyoto@gmail.com>
2019-05-16 19:16:17 +0900

    スキルをリスト表示に変更

--- containers/skills.vue
+++ containers/skills.vue
@@ -2,75 +2,38 @@
   <section id="skills" class="skills-wrapper">
     <pageTitle title="Skills" />
     <div class="skills">
-      <box v-for="(skill,index) in skills" :key="index" :item="skill">
-        {{ "★".repeat(skill.level) }}{{ "☆".repeat(3-skill.level) }}
-      </box>
+      <ul>
+        <li v-for="(skill,index) in skills" :key="index">
+          {{ skill.name }}
+        </li>
+      </ul>
     </div>
   </section>
 </template>
 
 <script>
 import pageTitle from '@/components/pageTitle'
-import box from '@/components/box'
-
 export default {
   name: 'Skills',
   components: {
-    pageTitle,
-    box
+    pageTitle
   },
   data() {
     return {
       skills: [
-        {
-          name: 'AWS',
-          content:
-            'EC2やECS、RDS、Lambda、CloudFront、Route53、RekognitionといったAWSの主要なサービスを使用したことがあります。',
-          level: 3
-        },
-        {
-          name: 'GitHub',
-          content:
-            'rebaseやcherry-pickを自由に使いこなすレベルです。また、プルリクエスト、レビューといった複数人での開発の経験があります。',
-          level: 3
-        },
-        {
-          name: 'Vue.js / Nuxt.js',
-          content:
-            'えもじっくやポートフォリオのフロントエンドに使用しています。また、pixivのハッカソンではNuxt.jsを使用し優勝しました。',
-          level: 3
-        },
-
-        {
-          name: 'Docker',
-          content:
-            'Dockerやdocker-composeを用いて、コンテナ環境での開発を行っています。',
-          level: 3
-        },
-        {
-          name: 'Circle CI',
-          content:
-            'テストや各種クラウドサービスへのデプロイをCI上で行っています。',
-          level: 3
-        },
-        {
-          name: 'Firebase',
-          content:
-            'Firebase Authenticationを使用したログイン機能の実装やJWTを使ったサーバーサイドとの連携の実装をしました。また、Cloud Firestoreを用いたデータベースの構築も行いました。',
-          level: 2
-        },
-        {
-          name: 'Django',
-          content:
-            'Django REST Frameworkを使ったバックエンドAPIの実装を行いました。また、Firebase Authenticationと連携させて認証ありAPIの実装も行いました。',
-          level: 2
-        },
-        {
-          name: 'GCP',
-          content:
-            'Postsに表示する投稿を返すAPIをGoogle App Engineにデプロイしていています。',
-          level: 1
-        }
+        { name: 'Golang' },
+        { name: 'Python' },
+        { name: 'JavaScript (ES6)' },
+        { name: 'C' },
+        { name: 'Java' },
+        { name: 'AWS' },
+        { name: 'GitHub' },
+        { name: 'Vue.js / Nuxt.js' },
+        { name: 'Docker' },
+        { name: 'Circle CI' },
+        { name: 'Firebase' },
+        { name: 'Django' },
+        { name: 'GCP' }
       ]
     }
   }
@@ -79,15 +42,18 @@ export default {
 
 <style lang='scss' scoped>
 @import '@/assets/styles/_mixin.scss';
-
 .skills-wrapper {
   @include section;
 }
-
 .skills {
   @include box-wrapper;
 }
-
+li {
+  width: 100%;
+  text-align: left;
+  font-size: 16px;
+  padding: 4px 0;
+}
 /*タブレット*/
 @media (max-width: 1100px) {
   .skills {
