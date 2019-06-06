33218fa9f36c15e66a17101304bda16b9eece193
Naoki Kishi <naokikishi.kyoto@gmail.com>
2019-06-06 17:09:20 +0900

    Add title border

--- components/pageTitle.vue
+++ components/pageTitle.vue
@@ -23,16 +23,32 @@ export default {
   font-weight: normal;
   color: $text-color;
   font-size: 48px;
-  width: 33%;
-  margin: 0;
+  margin: 0 10%;
   top: 50px;
 }
 
+.title:after {
+  display: block;
+  height: 2px;
+  content: '';
+  background: linear-gradient(
+    to right,
+    rgba(47, 128, 237, 0.7) 0%,
+    rgba(86, 204, 242, 0.7) 100%
+  );
+}
 @media (max-width: 1250px) {
   .title {
-    padding: 16px 0;
+    padding: 16px 0 32px 0;
     font-size: 36px;
     width: 100%;
+    margin: 0 15%;
+  }
+}
+
+@media (max-width: 670px) {
+  .title {
+    margin: 0 10vw;
   }
 }
 </style>
