8d0fd9f723fd806d0f8fa707129cd629d364257a
Naoki Kishi <naokikishi.kyoto@gmail.com>
2021-03-16 12:46:24 +0900

    setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,79 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/order': [
+      'error',
+      {
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'parent',
+            position: 'after'
+          }
+        ],
+        'newlines-between': 'always'
+      }
+    ],
+    'import/no-default-export': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        printWidth: 100,
+        tabWidth: 2,
+        semi: false
+      }
+    ],
+    semi: ['error', 'never'],
+    indent: ['error', 2]
+  },
+  // Next.js向けのページコンポーネントはdefault exportしか使えないなので除外
+  overrides: [
+    {
+      files: ['src/pages/**/*.tsx'],
+      rules: {
+        'import/no-default-export': 'off'
+      }
+    }
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      typescript: {
+        project: '.'
+      }
+    }
+  }
+}
