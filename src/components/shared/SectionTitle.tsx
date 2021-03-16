26af1cbe98f25e630f47f19113e6945204555c15
Naoki Kishi <naokikishi.kyoto@gmail.com>
2021-03-16 20:55:46 +0900

    marginを調整

--- src/components/shared/SectionTitle.tsx
+++ src/components/shared/SectionTitle.tsx
@@ -7,7 +7,7 @@ export const SectionTitle = ({ title, subtitle }: Props) => {
   return (
     <div>
       <h1 className="text-3xl text-blue text-center font-bold">{title}</h1>
-      <span className="text-center block text-gray-light">{subtitle}</span>
+      <span className="text-center block text-gray-light mt-1">{subtitle}</span>
     </div>
   )
 }
