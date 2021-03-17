22e124ba622f081759772b62d999baa06c102888
Naoki Kishi <naokikishi.kyoto@gmail.com>
2021-03-18 01:13:42 +0900

    titleを指定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <Head />
+        <Head title="p1ass's portfolio" />
         <body className="tracking-wide">
           <Main />
           <NextScript />
