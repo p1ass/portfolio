65106c31c1f58b857ad2a7da7dd73723edc5392d
Naoki Kishi <naokikishi.kyoto@gmail.com>
2021-03-18 23:17:41 +0900

    paddingを調整

--- src/components/shared/WhiteBlock.tsx
+++ src/components/shared/WhiteBlock.tsx
@@ -1,6 +1,6 @@
 export const WhiteBlock: React.FC = ({ children }) => {
   return (
-    <div className="bg-white flex flex-col mx-4 my-8 px-4 py-4 rounded-lg md:my-4 md:w-full">
+    <div className="flex flex-col mx-4 my-8 px-4 py-8 bg-white rounded-lg md:my-4 md:w-full">
       {children}
     </div>
   )
