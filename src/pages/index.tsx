d0fe14d819d4b2269fe2ac88fd0b5d8a6789d3ed
Naoki Kishi <contact@p1ass.com>
2021-08-02 16:38:57 +0900

    add eslint-config-next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Index() {
         <title>p1ass&apos;s portfolio</title>
       </Head>
       <div>
-        <main className="bg-background container mb-16 lg:max-w-5xl">
+        <main className="container mb-16 bg-background lg:max-w-5xl">
           <AboutSection></AboutSection>
           <CareerSection></CareerSection>
           <PresentationsSection></PresentationsSection>
