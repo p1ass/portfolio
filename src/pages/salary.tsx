ac01c31fe88fd42582a5b3026cf15c8eec40e8cd
Naoki Kishi <contact@p1ass.com>
2021-12-14 21:20:06 +0900

    a

--- src/pages/salary.tsx
+++ src/pages/salary.tsx
@@ -18,19 +18,46 @@ export default function Salary() {
             </thead>
             <tbody>
               <tr>
-                <td className="px-4">2020 early</td>
-                <td className="px-4">2000/h</td>
-                <td className="px-4">0</td>
+                <td className="px-4" align="right">
+                  2020/01
+                </td>
+                <td className="px-4" align="right">
+                  2000/h
+                </td>
+                <td className="px-4" align="right">
+                  0
+                </td>
               </tr>
               <tr>
-                <td className="px-4">2020 later</td>
-                <td className="px-4">2200/h</td>
-                <td className="px-4">0</td>
+                <td className="px-4" align="right">
+                  2020/04
+                </td>
+                <td className="px-4" align="right">
+                  2200/h
+                </td>
+                <td className="px-4" align="right">
+                  0
+                </td>
               </tr>
               <tr>
-                <td className="px-4">2021</td>
-                <td className="px-4">{Buffer.from('NjAw', 'base64').toString('ascii')}</td>
-                <td className="px-4">0</td>
+                <td className="px-4" align="right">
+                  2021/04
+                </td>
+                <td className="px-4" align="right">
+                  {Buffer.from('NiwwMTAsMDAwCg==', 'base64').toString('ascii')}
+                </td>
+                <td className="px-4" align="right">
+                  0
+                </td>
+              </tr>
+              <tr>
+                <td className="px-4" align="right">
+                  2021/12
+                </td>
+                <td className="px-4">-</td>
+                <td className="px-4" align="right">
+                  {Buffer.from('MjQ1LDAwMAo=', 'base64').toString('ascii')}
+                </td>
               </tr>
             </tbody>
           </table>
